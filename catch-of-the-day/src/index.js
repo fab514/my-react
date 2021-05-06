bde31c71dbffd14da2740a332c4daab51c381202
Felicia Brenner <felicia.brenner514@gmail.com>
2021-05-06 00:07:17 -0500

    feat(index.js): imported App.js to the index file

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'; // goes into node module folder. Imports entire react package
 import { render } from 'react-dom'; // Only imports a branch of react. Looking for a specific method (render method)
 import StorePicker from './components/StorePicker';
-import '../';
+import App from './components/App';
 
 // component should have a self closing tags
-render(<StorePicker />, document.querySelector('#main')); // where in html the React will go
+render(<App />, document.querySelector('#main')); // where in html the React will go
 // takes in jsx (similar to html), and the mounting point(the dom element). You can also use document.getElementById
