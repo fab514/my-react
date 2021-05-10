19d81daa3dd350a156308fbeb1d39bd3c2ec83d4
Felicia Brenner <felicia.brenner514@gmail.com>
2021-05-10 11:10:05 -0500

    feat: created a way to transfer to the store site from the create store

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
-class StorePicker extends React.Component { 
-    // classes in React should start with a capital letter and always should have a method
+class StorePicker extends React.Component {
+    myInput = React.createRef();
+    // Use ref in react allows us to reference dom nodes on the page
+    
+    goToStore = event => {
+        // 1. Stop the form from submitting
+        event.preventDefault(); 
+        // 2. Get the text from the input 
+        const storeName = this.myInput.current.value;
+        // 3. Change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`);
+    };
+
+    
     render() { // method put inside of a componant
-        // in jsx use className instead of class
-        return ( 
-            // you can only return one parent element(not sibling element), you can use react fragment to put siblings inside
-            <React.Fragment> 
-                <form className="store-selector">
-                    {/* This is how you comment in jsx. You can't put a comment in parent with a sibling. You must nest inside of the child element */} 
-                    <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" />
-                    <button type="submit">Visit Store</button>
-                </form> 
-            </React.Fragment>
-        ) // Parentheses will run first
+        return (
+            <form className="store-selector" onSubmit={this.goToStore}>
+                {/* This is how you comment in jsx. You can't put a comment in parent with a sibling. You must nest inside of the child element */}
+                <h2>Please Enter A Store</h2>
+                <input 
+                    type="text" 
+                    ref={this.myInput}
+                    required placeholder="Store Name" 
+                    defaultValue={getFunName()} 
+                />
+                <button type="submit">Visit Store</button>
+            </form>
+
+        ); // Inside parentheses will run first
     }
-} 
+}
 
 export default StorePicker;
