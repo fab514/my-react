5d18f767ce274078aa9b7dad326b85197ce86c12
Felicia Brenner <felicia.brenner514@gmail.com>
2021-05-10 11:07:23 -0500

    fix: adjust routes to js files

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'; // goes into node module folder. Imports entire react package
-import { render } from 'react-dom'; // Only imports a branch of react. Looking for a specific method (render method)
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import React from "react"; // goes into node module folder. Imports entire react package
+import { render } from "react-dom"; // Only imports a branch of react. Looking for a specific method (render method)
+import Router from "./components/Router";
+import "./css/style.css";
 
-// component should have a self closing tags
-render(<App />, document.querySelector('#main')); // where in html the React will go
+
+render(<Router />, document.querySelector("#main")); // where in html the React will go
 // takes in jsx (similar to html), and the mounting point(the dom element). You can also use document.getElementById
