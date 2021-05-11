36cfba3252222d977649b50918645f9b5b786434
Felicia Brenner <felicia.brenner514@gmail.com>
2021-05-10 20:50:06 -0500

    feat(App.js): connected to the Fish Form

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,19 +2,37 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
- render() {
-    return(
-        <div className="catch-of-the-day">
-            <div className="menu">
-                <Header tagline="Fresh Seafood Market" age={100} />
+    // methods of the state and the state needs to be in the same component
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish => { // how to update a piece of state
+        // 1. Take a copy of existing state
+        const fishes = {...this.state.fishes }; // copy
+        // 2. add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes object to state
+        this.setState({ fishes })
+    };
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
+
+    render() {
+        return(
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market" />
+                </div>
+                <Order />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
-            <Order />
-            <Inventory />
-        </div>
-    );
- }
+        );
+    }
 
 }
 
