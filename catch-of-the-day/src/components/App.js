6bca5c344cf5e40ff46926845ab08b58e91132bf
Felicia Brenner <felicia.brenner514@gmail.com>
2021-05-16 16:27:39 -0500

    feat(App.js): created an add to order so any fish clicked will be added to the orderform

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
     // methods of the state and the state needs to be in the same component
@@ -12,7 +13,7 @@ class App extends React.Component {
     };
     addFish = fish => { // how to update a piece of state
         // 1. Take a copy of existing state
-        const fishes = {...this.state.fishes }; // copy
+        const fishes = { ...this.state.fishes }; // copy
         // 2. add our new fish to that fishes variable
         fishes[`fish${Date.now()}`] = fish;
         // 3. Set the new fishes object to state
@@ -22,13 +23,33 @@ class App extends React.Component {
         this.setState({ fishes: sampleFishes });
     };
 
+    addToOrder = key => {
+        // 1. take a copy of state
+        const order = { ...this.state.order };
+        // 2. Either add to order, or update in our order
+        order[key] = order[key] + 1 || 1;
+        // 3. Call setState to update our state object
+        this.setState({ order });
+    };
+
     render() {
-        return(
+        return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish
+                                key={key}
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder}
+                            />
+                        ))}
+                        {/*Turn the fish input into an array which each will show the fish emoji, key (specific to react) will give each a unique identifier  */}
+                    </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
