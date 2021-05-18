b5bd83b78d11d1c29a0319443320d7a871445b70
Felicia Brenner <felicia.brenner514@gmail.com>
2021-05-18 15:02:46 -0500

    feat(Inventory.js): mount an edit fish form in inventory

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,5 +1,6 @@
 import React from "react";
-import AddFishForm from './AddFishForm';
+import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm"
 
 // Inventory.js is a componant for the inventory section of application
 class Inventory extends React.Component {
@@ -7,6 +8,14 @@ class Inventory extends React.Component {
         return (
             <div className="inventory">
                 <h2>Inventory!!</h2>
+                {Object.keys(this.props.fishes).map(key => 
+                    <EditFishForm 
+                    key={key} 
+                    index={key} 
+                    fish={this.props.fishes[key]}
+                    updateFish={this.props.updateFish} 
+                    />
+                )}
                 <AddFishForm addFish={this.props.addFish} /> 
                 <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
