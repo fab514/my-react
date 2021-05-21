90d0fb36744cfb92bca3ba323f32340f46987eaf
felicia <you@example.com>
2021-05-21 12:57:47 -0500

    feat(Order.js): added a button to each fish adder to order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -13,14 +13,15 @@ class Order extends React.Component {
         if(!isAvailable) {
             return (
                 <li key={key}>
-                Sorry {fish ? fish.name : 'fish'} is no longer available
+                    Sorry {fish ? fish.name : 'fish'} is no longer available
                 </li>
             );
         }
         return (
             <li key={key}>
-            {count} lbs {fish.name}
-            {formatPrice(count * fish.price)}
+                {count} lbs {fish.name}
+                {formatPrice(count * fish.price)}
+                <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
             </li>
         );
     }
